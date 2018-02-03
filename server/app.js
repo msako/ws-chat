7ef2553724ea1bdf7c76dd3b7949f4765bb7652d
Masataka <sako@acemmg.com>
2018-02-03 10:01:23 -0800

    Use room in socket

--- server/app.js
+++ server/app.js
@@ -14,9 +14,14 @@ io.adapter(redis({ host: process.env.REDIS_HOST || '127.0.0.1', port: 6379 }))
 io.on('connection', socket => {
   console.log(socket.id, ' is connected.')
 
-  socket.on('SEND_MESSAGE', data => {
+  socket.on('room', room => {
+    console.log(room)
+    socket.join(room)
+  })
+
+  socket.on('send_message', data => {
     data.id = Date.now()
 
-    io.emit('RECEIVE_MESSAGE', data)
+    io.sockets.in(data.room).emit('receive_message', data)
   })
 })
