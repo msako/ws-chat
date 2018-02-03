1e584abb4632c394aea6d9cb4d99d75089870836
Masataka <sako@acemmg.com>
2018-02-03 09:58:11 -0800

    Add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Chat from './chat'
+import Home from './home'
 
 const App = () => (
   <MuiThemeProvider>
-    <Chat />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/chat/:id" component={Chat} />
+      </Switch>
+    </Router>
   </MuiThemeProvider>
 )
 
