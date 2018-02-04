dddbb7df107548e9fb96b5a015b18ba29ecebcb1
Masataka <sako@acemmg.com>
2018-02-03 22:21:54 -0800

    [refactor]

--- frontend/src/chat/components/messageList.js
+++ frontend/src/chat/components/messageList.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react'
-import { List, ListItem } from 'material-ui'
+import React from 'react'
+import { List, ListItem } from 'material-ui/List'
+import Card from 'material-ui/Card'
 import format from 'date-fns/format'
 import Message from './message'
 
-class MessageList extends Component {
-  formatDate(timestamp) {
-    return format(new Date(timestamp), 'MM/DD HH:mm')
-  }
-  renderList() {
-    return (
-      <List>
-        {this.props.messages.map((message, index) => {
-          message.date = this.formatDate(message.id)
+const formatDate = timestamp => format(new Date(timestamp), 'MM/DD HH:mm')
 
-          return (
-            <ListItem key={index} innerDivStyle={styles.messagelist}>
-              <Message key={index} message={message} styles={styles} />
-            </ListItem>
-          )
-        })}
-      </List>
-    )
-  }
+const renderList = props => (
+  <Card>
+    <List>
+      {props.messages.map((message, index) => {
+        message.date = formatDate(message.id)
 
-  renderNotice() {
-    return (
-      <List>
-        <ListItem disabled={true}>
-          <span>Start chat...</span>
-        </ListItem>
-      </List>
-    )
-  }
+        return (
+          <ListItem key={index} innerDivStyle={styles.messagelist}>
+            <Message
+              key={index}
+              message={message}
+              styles={props.styles || styles}
+            />
+          </ListItem>
+        )
+      })}
+    </List>
+  </Card>
+)
 
-  render() {
-    return this.props.messages.length > 0
-      ? this.renderList()
-      : this.renderNotice()
-  }
-}
+const renderNotice = (
+  <Card>
+    <List>
+      <ListItem disabled={true}>
+        <span>Start chat...</span>
+      </ListItem>
+    </List>
+  </Card>
+)
+
+const MessageList = props =>
+  props.messages.length > 0 ? renderList(props) : renderNotice
 
 const styles = {
   date: {
     fontSize: '0.8em',
     color: 'gray',
-    marginRight: '12px',
-    paddingLeft: '12px'
+    marginRight: 12,
+    paddingLeft: 12
   },
   author: {
     fontWeight: 'bold'
   },
   messagelist: {
-    padding: '4px'
+    padding: 4
   }
 }
 
